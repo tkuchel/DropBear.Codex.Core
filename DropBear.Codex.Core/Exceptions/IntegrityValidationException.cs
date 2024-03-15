7969b84d46b2a224df9018493fb41d3cff3f09d6
DropBear <tkuchel@users.noreply.github.com>
2024-03-15 15:52:08 +1100

    Slight refactor to try and improve performance with regards to cryptographic computations.

---
+++ DropBear.Codex.Core/Exceptions/IntegrityValidationException.cs
@@ -0,0 +1,19 @@
+﻿namespace DropBear.Codex.Core.Exceptions;
+
+/// <summary>
+///     Represents errors that occur during integrity validation of a payload.
+/// </summary>
+public class IntegrityValidationException : Exception
+{
+    public IntegrityValidationException() { }
+
+    public IntegrityValidationException(string message)
+        : base(message)
+    {
+    }
+
+    public IntegrityValidationException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
